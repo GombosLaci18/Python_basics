54a06a0f95078e2736e45faa0dde2cfe8c437718
Lacapapa <gomboslaci2005@gmail.com>
2024-10-02 13:51:31 +0200

    Learn about for and while loops

--- 2024.10.02.py
+++ 2024.10.02.py
@@ -11,3 +11,21 @@ b: bool = False
     else:
        print("neither a or b is True")
 """
+# Loopolás:
+
+# for loop:
+""" b = [0, 1, 2, 3, 4]
+for i in range(5):
+    print(i)
+    pass """
+
+# while loop:
+""" message: str = "Give me your answer! "
+while True:
+    answer: str = input(message)
+    if len(answer) >= 3:
+        print("Thank you!")
+        break
+        # a while loop megtörése(kilép)
+    else:
+        message = "Please give me at least 3 characters!!! " """
