9efb1f185059b48ce2bf7bac917f6896515ab896
Lacapapa <gomboslaci2005@gmail.com>
2024-10-02 13:20:20 +0200

    Learned about if statement

---
+++ 2024.10.02.py
@@ -0,0 +1,13 @@
+a: bool = True
+b: bool = False
+
+# Az if parancsok gyakorlása
+""" if a == True:
+    print("a is True")
+    if b == True:
+    print("b is True")
+    elif b:
+       print("b is True)
+    else:
+       print("neither a or b is True")
+"""
